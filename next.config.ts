9d08f0e6668e1494c5f5b4253ccfdec4973bc23e
Your Name <your.email@example.com>
2025-05-18 03:23:30 +0300

    Fix merge conflict in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-<<<<<<< HEAD
     domains: [
       'hagzzstorage.blob.core.windows.net',
       'ekyerljzfokqimbabzxm.supabase.co'
     ],
-=======
-    domains: ['hagzzstorage.blob.core.windows.net'],
->>>>>>> bca22f5650d9d75baeb93f1e7ba3fc2055bf8bd6
   },
   env: {
     AZURE_STORAGE_ACCOUNT_NAME: process.env.AZURE_STORAGE_ACCOUNT_NAME,
