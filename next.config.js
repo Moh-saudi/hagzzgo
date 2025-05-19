64ffe84357ce2f135a9fc48854cb4dfeac3de177
Moh-saudi <mo.saudi19@gmail.com>
2025-05-19 12:35:35 +0300

    Fix CSS merge conflicts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      'ekyerljzfokqimbabzxm.supabase.co',
+      'firebasestorage.googleapis.com'
+    ],
+  },
+  env: {
+    AZURE_STORAGE_ACCOUNT_NAME: process.env.AZURE_STORAGE_ACCOUNT_NAME || '',
+    AZURE_STORAGE_SAS_TOKEN: process.env.AZURE_STORAGE_SAS_TOKEN || '',
+    AZURE_STORAGE_CONTAINER_NAME: process.env.AZURE_STORAGE_CONTAINER_NAME || '',
+  },
+  // Add other configurations here
   reactStrictMode: true,
   swcMinify: true,
 }
